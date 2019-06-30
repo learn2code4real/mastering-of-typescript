67b8bdd87afcc0d8969dfb687b9a9ac480fc54cd
sohag1990 <whoshakhawat@gmail.com>
2019-07-01 01:28:51 +0600

    types

---
+++ types.ts
@@ -0,0 +1,31 @@
+// Number type
+var numberOfProducts: number = 13;
+var pricePerProduct: number = 1.5;
+
+var totalPrice: number = numberOfProducts* pricePerProduct;
+
+console.log("Number of products are "+ numberOfProducts);
+console.log("Total Price "+ totalPrice);
+
+// String
+var productName: string = "Printer"
+
+var productType: string = "computer accessoris"
+
+console.log("We have a product name "+ productName + " and price per pice is "+ pricePerProduct+". We have "+ numberOfProducts + " in our stock");
+
+// Boolean
+
+var available: boolean = false;
+
+console.log(available);
+
+// void
+
+function helloWorld(): void {
+    console.log("hello world")
+}
+
+console.log("Is this function return anything?: "+helloWorld())
+
+
